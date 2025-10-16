6071d280bd664bc354360a6e21f211989916eb3b
Patryk Glowczynski <patryk.gglo@gmail.com>
2025-10-16 10:00:00 +0200

    Mobile: Global configuration

---
+++ mobile/src/config.js
@@ -0,0 +1,4 @@
+// M1: Improper Credential Usage - Hardcoded secret in source code
+export const API_KEY = "SECRET_ADMIN_KEY_12345";
+export const API_URL = "http://10.0.2.2:5000"; // Android Emulator to Localhost
+// export const API_URL = "http://localhost:5000"; // For iOS/Web
